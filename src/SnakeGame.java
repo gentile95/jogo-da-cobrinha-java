47efddb6904e0eb29c1516eb48a8ccffd3d45dcf
Lucas Gentile <lucas.gentile95@gmail.com>
2023-11-19 14:11:30 -1200

    Updates e melhorias

--- src/SnakeGame.java
+++ src/SnakeGame.java
@@ -4,6 +4,7 @@ import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.util.ArrayList;
